481fac12aff1ccaa344b236440d1f481f6a32855
Dearo-Yam <70773145+Dearo-Yam@users.noreply.github.com>
2024-01-21 22:16:01 -0800

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'asker';
+  sendYes(){
+    console.log('hello')
+  }
+  handleHover() {
+    const button = document.getElementById('NoBtn');
+    if (button) {
+      // Generate new random values for left and top positions
+      const leftPosition = Math.ceil(Math.random() * 500);
+      const topPosition = Math.ceil(Math.random() * 500);
+  
+      // Apply the new transform values with px units and add a transition effect
+      button.style.transition = 'transform 0.3s ease'; // Adjust the duration and timing function as needed
+      button.style.transform = `translate(${leftPosition}px, ${topPosition}px)`;
+      const txt = document.getElementById('display')
+      if(txt){
+        txt.style.display = 'block'
+        setTimeout(function(){txt.style.display = 'none'},3000)
+      }
+      console.log('Hello, I am being hovered');
+    }
+  }
+  displayText(){
+    console.log('wow you clicked me')
+  }
 }
